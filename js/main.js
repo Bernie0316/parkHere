dbccb49d9f051e997833dbe9f777c79403f55a8e
Bernie Zhong <chu23008@byui.edu>
2025-06-19 03:23:14 +0800

    最近三個停車場功能完成

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 import { loadHeaderFooter } from "./utils.mjs";
-// import ExternalServices from "./ExternalService.mjs";
-// import ParkList from "./parkList.mjs";  
 import "./locator.mjs"; // 引入定位功能
 
 loadHeaderFooter();
-
-// const listElement = document.querySelector(".park-list");
-// const service = new ExternalServices();
-// const parkList = new ParkList(service, listElement);
-// parkList.init();
